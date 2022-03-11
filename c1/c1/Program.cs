cc2ecd3f390bc10d47ece4f7517529233c938a8f
Panos Skoufos <pskoufos@gmail.com>
2022-03-11 18:11:35 +0200

    2

--- c1/c1/Program.cs
+++ c1/c1/Program.cs
@@ -1,10 +1,53 @@
-﻿class Program
+﻿namespace c1
 {
-    static Main(string[] args )
+    using System; 
+
+    public delegate void myMessage(string input);
+
+    class Program
     {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi i am live....");
+            Console.ReadKey();
+
+            myMessage my = classA.displayA;
+            my("hi there ...");
+
+            my = classb.displayB;
+            my("hi there ...");
+
+            my = (string s) => Console.WriteLine("Lamda expresion  " + s);
+            my("hi there ...."); 
+
+            Console.ReadKey();
+        }
+
 
     }
 
 
-}
+    public class classA
+    {
+        public static void displayA(string message){
+            Console.WriteLine("classA --> {0}", message);
 
+        }
+        
+        
+    }
+
+    public class classb
+    {
+        public static void displayB(string message)
+        {
+            Console.WriteLine("classB --> {0}", message);
+
+        }
+
+
+    }
+
+
+
+}// namespace 
